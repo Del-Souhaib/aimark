8c0d425e94d085176cf2f9edf71d35aa38369346
souhaib <del.souhaub@gmail.com>
2022-05-06 13:22:09 +0000

    push

---
+++ authenticate/views.py
@@ -0,0 +1,37 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+def loginauth(request):
+    if request.user.is_authenticated:
+        return redirect('/admins')
+    else:
+        return render(request, 'auth/login.html')
+
+
+def loginclick(request):
+    if (request.method == 'POST'):
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('/admins')
+        # A backend authenticated the credentials
+        else:
+            messages.error(request, "Email ou mot de passe est incorrect")
+            return redirect('/admins/login')
+    # No backend authenticated the credentials
+    else:
+        return redirect('/admins/login')
+
+
+def logoutclick(request):
+    if (request.method == 'POST' and request.user.is_authenticated):
+        logout(request)
+        return redirect('/admins/login')
+    else:
+        return redirect('/admins')
